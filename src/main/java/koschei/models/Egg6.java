432aa16bcd4ba93e59b34b2c75b17cfcb7fcd7f1
Eaagle <vvvzlnr@gmail.com>
2025-03-11 22:38:48 +0300

    исправлено

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,12 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private Needle7 needle7;
-
     @Autowired
-    public void setNeedle7(Needle7 needle7) {
-        this.needle7 = needle7;
-    }
+    private Needle7 needle7;
 
     @Override
     public String toString() {
